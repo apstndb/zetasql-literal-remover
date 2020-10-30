782c1b13fed0e43a95edc74aca4b792e8a49c0a4
HATATANI Shinta <gamaguchi@gmail.com>
2020-10-30 16:00:46 +0900

    Update example

--- example.sql
+++ example.sql
@@ -1,6 +1,6 @@
 SELECT *, 0, 0.0, TRUE, FALSE, "", b"", NUMERIC "0", TIMESTAMP "1970-01-01T00:00:00Z", DATE "1970-01-01",
-       STRUCT(1), STRUCT(CURRENT_DATE()),
-       [1], [CURRENT_DATE()],
+       STRUCT(1), STRUCT(CURRENT_DATE()), -- a struct constructor containing only literals is aliteral
+       [1], [CURRENT_DATE()],             -- a array constructor containing only literals is a literal
 FROM Singers AS s JOIN@{FORCE_JOIN_ORDER=TRUE} Albums AS a
 ON s.SingerId = a.Singerid
 WHERE s.LastName LIKE '%x%' AND a.AlbumTitle LIKE '%love%';
